dae0a1e4991d6cf746eec5f51b22f1d80c5176d4
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2023-06-04 18:10:20 +0100

    css

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import './TodoList.css';
 
 function TodoList(){
-  return(<h1>Hello world</h1>)
+  return(
+    <div>
+      <h1>To Do List</h1>
+      <form action="">
+        <input type="text" placeholder="Add new task"/>
+      <button type="submit">Add</button>
+      </form>
+      <div className="listTask">
+        <div className="item">
+          <span>Tarefa de exemplo</span>
+          <button>Deletar</button>
+        </div>
+        <div className="item completo">
+          <span>Tarefa de exemplo</span>
+          <button>Deletar</button>
+        </div>
+      </div>
+    </div>
+  
+  )
 }
 
 export default TodoList;
